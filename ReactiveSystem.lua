fafc9a83e4d1b65ad207fde09830de4e109b7ebf
YanqingXu <xyq949587362xyq007@163.com>
2025-03-31 11:16:13 +0800

    Update ReactiveSystem.lua

--- ReactiveSystem.lua
+++ ReactiveSystem.lua
@@ -11,7 +11,7 @@
     5. 计算属性懒计算：访问计算属性时，如果有缓存则直接返回，否则调用getter重新计算
                       当计算属性依赖的响应式对象发生变化时，清除缓存
 --]]
-local Stack = require("Stack")
+local Stack = require("xf.engine.Stack")
 local ReactiveSystem = {
     --[[ 
         副作用函数表: effects为副作用函数，key为proxyTable，value为key对应的副作用函数列表
@@ -32,18 +32,6 @@ local ReactiveSystem = {
         }
     --]]
     effects = {},
-
-    --[[
-        计算属性缓存: computedTable为计算属性，本质是一个代理表，key固定为"value"，value为计算属性的值
-        为实现懒计算而设计出的一个全局缓存表
-        数据结构：
-        { 
-            computedTable1 = value1, 
-            computedTable2 = value2, 
-            ... 
-        }
-    --]]
-    caches = {},
 
     --[[
         直接依赖关系表: deps为所有计算属性的依赖关系表，用于依赖收集
@@ -78,9 +66,7 @@ local isComputed = nil      -- 是否是计算属性
 
 local reactive = nil        -- 创建Reactive对象
 local ref = nil             -- 创建Ref对象
-
 local computed = nil        -- 创建计算属性
-local clearComputed = nil   -- 清理计算属性
 
 local watch = nil           -- 监视响应式数据变化
 local unwatch = nil         -- 取消监视
@@ -99,39 +85,30 @@ local computedTableStack = Stack.new()
 local Proxy = {}
 Proxy.__index = function(proxyTable, key)
     if key == "_real" then
-        return rawget(proxyTable._real, key)
+        return rawget(proxyTable, key)
     end
 
-    -- 检测是否有副作用函数绑定
-    local effectFuncPtr = effectFuncPtrStack:top()
-    if effectFuncPtr then
-        ReactiveSystem:subscribe(proxyTable, key, effectFuncPtr)
-    end
+    -- 建立依赖关系：effect依赖proxyTable
+    ReactiveSystem:subscribe(proxyTable, key, effectFuncPtrStack:top())
 
-    -- 建立依赖关系
-    ReactiveSystem:buildDeps(proxyTable, key, computedTableStack:top())
+    -- 建立依赖关系：computedTable依赖proxyTable
+    ReactiveSystem:link(proxyTable, key, computedTableStack:top())
 
-    return rawget(proxyTable._real, key)
+    -- 返回访问的原始表的值
+    return proxyTable._real[key]
 end
-Proxy.__newindex = function(proxyTable, key, value)
-    -- 递归创建Reactive对象
-    if type(value) == "table" then
-        if not isReactive(value) then
-            value = reactive(value)
-        end
+Proxy.__newindex = function(proxyTable, key, newValue)
+    local oldValue = proxyTable._real[key]
+    if not oldValue then
+        ReactiveSystem:subscribe(proxyTable, key, effectFuncPtrStack:top())
+        ReactiveSystem:link(proxyTable, key, computedTableStack:top())
     end
-
-    local oldValue = rawget(proxyTable._real, key)
 
     -- 当数据发生变化时
-    if oldValue ~= value then
-        rawset(proxyTable._real, key, value)
-
-        -- 触发依赖关系，清除所有依赖proxyTable的计算属性的缓存
-        ReactiveSystem:triggerDeps(proxyTable, key)
-
-        -- 通知与之绑定的副作用函数重新执行
-        ReactiveSystem:publish(proxyTable, key, oldValue)
+    if oldValue ~= newValue then
+        proxyTable._real[key] = newValue
+        ReactiveSystem:propgate(proxyTable, key)            -- 传播依赖关系
+        ReactiveSystem:notify(proxyTable, key, oldValue)    -- 通知effect重新执行
     end
 end
 
@@ -147,7 +124,7 @@ end
 local Computed = {}
 Computed.__index = function(computedTable, key)
     if key == "_real" then
-        return rawget(computedTable._real, key)
+        return rawget(computedTable, key)
     end
 
     -- 计算属性只有一个key，固定为"value"
@@ -155,66 +132,52 @@ Computed.__index = function(computedTable, key)
         return nil
     end
 
-    -- 检测绑定副作用函数
-    local effectFuncPtr = effectFuncPtrStack:top()
-    if effectFuncPtr then
-        ReactiveSystem:subscribe(computedTable, key, effectFuncPtr)
-    end
+    -- 建立依赖关系：effect依赖computedTable
+    ReactiveSystem:subscribe(computedTable, key, effectFuncPtrStack:top())
 
-    -- 建立依赖关系
-    ReactiveSystem:buildDeps(computedTable, key, computedTableStack:top())
+    -- 建立依赖关系：computedTable依赖computedTable
+    ReactiveSystem:link(computedTable, key, computedTableStack:top())
 
-    -- 调用getter函数之前，先将计算属性对象压入栈中，以便后续建立依赖关系
-    if not computedTableStack:hasElement(computedTable) then
-        computedTableStack:push(computedTable)
-    end
+    -- 调用getter函数之前，先将计算属性对象压入栈中，以便在getter中建立依赖关系
+    computedTableStack:push(computedTable)
 
     -- 计算属性懒计算
-    if not ReactiveSystem.caches[computedTable] then
-        -- getterValueIsNil为true时，表示上一次访问时getter函数返回了nil
-        -- 因此本次访问时直接返回nil，无须再调用getter
-        if computedTable._real.getterValueIsNil then
-            if not computedTableStack:isEmpty() then                
-                computedTableStack:pop()
-            end
-
-            return nil
-        end
-       
+    if computedTable._real._recompute then
         -- 调用getter函数
-        ReactiveSystem.caches[computedTable] = computedTable._real.getter()
+        local oldValue = computedTable._real._cachedValue
+        local newValue = computedTable._real.getter(oldValue)
+        computedTable._real._cachedValue = newValue
 
-        -- 当计算属性的getter函数返回nil时，标记为true
-        computedTable._real.getterValueIsNil = ReactiveSystem.caches[computedTable] == nil
+        -- 标记不需要重新计算
+        computedTable._real._recompute = false
     end
 
-    if not computedTableStack:isEmpty() then
-        computedTableStack:pop()
-    end
+    -- 弹出栈顶元素
+    computedTableStack:pop()
 
     -- 返回计算属性的值
-    return ReactiveSystem.caches[computedTable]
+    return computedTable._real._cachedValue
 end
 
-Computed.__newindex = function(computedTable, key, value)
+Computed.__newindex = function(computedTable, key, newValue)
     -- 计算属性只有一个key，固定为"value"
     if "value" ~= key then
         return
     end
 
     -- 更新计算属性的值
-    local oldValue = ReactiveSystem.caches[computedTable]
-    ReactiveSystem.caches[computedTable] = value
+    local oldValue = computedTable._real._cachedValue
+    computedTable._real._cachedValue = newValue
 
     -- 如果计算数值有setter函数，则调用setter函数
     if computedTable._real.setter then
-        computedTable._real.setter(value)
+        computedTable._real.setter(newValue)
     end
 
     -- 通知与之绑定的副作用函数重新执行
-    if oldValue ~= value then
-        ReactiveSystem:triggerDeps(computedTable, key)
-        ReactiveSystem:publish(computedTable, key, oldValue)
+    if oldValue ~= newValue then
+        ReactiveSystem:propgate(computedTable, key)
+        ReactiveSystem:notify(computedTable, key, oldValue)
     end
 end
 
@@ -224,8 +187,9 @@ function Computed.new(getter, setter)
     realTable.getter = getter
     realTable.setter = setter
 
-    -- 如果getter函数返回nil，则标记为true，下次访问时直接返回nil，不再调用getter
-    realTable.getterValueIsNil = false
+    -- 缓存计算属性的值
+    realTable._cachedValue = nil
+    realTable._recompute = true
 
     local computedTable = {}
     computedTable._real = realTable
@@ -249,59 +213,75 @@ isComputed = function(obj)
     return getmetatable(obj) == Computed
 end
 
--- 建立依赖关系：firstKey可能是proxyTable，也可能是computedTable
-function ReactiveSystem:buildDeps(firstKey, secondKey, targetComputedTable)
-    if not targetComputedTable then
+-- 建立依赖关系：t可能是proxyTable，也可能是computedTable
+function ReactiveSystem:link(t, key, target)
+    if not target then
         return
     end
 
-    if not self.deps[firstKey] then
-        self.deps[firstKey] = {}
+    if not self.deps[t] then
+        self.deps[t] = {}
     end
 
-    if not self.deps[firstKey][secondKey] then
-        self.deps[firstKey][secondKey] = {}
+    if not self.deps[t][key] then
+        self.deps[t][key] = {}
     end
 
-    for _, computedTable in ipairs(self.deps[firstKey][secondKey]) do
-        if computedTable == targetComputedTable then
+    for _, v in ipairs(self.deps[t][key]) do
+        if v == target then
             return
         end
     end
 
-    table.insert(self.deps[firstKey][secondKey], targetComputedTable)
+    table.insert(self.deps[t][key], target)
 end
 
--- 递归触发依赖关系：firstKey可能是proxyTable，也可能是computedTable
-function ReactiveSystem:triggerDeps(firstKey, secondKey)
-    if not self.deps[firstKey] then
+-- 遍历依赖关系表，清理目标计算属性的依赖关系
+function ReactiveSystem:clearLink(computedTable)
+    for k, v in pairs(self.deps) do
+        for key, computedTables in pairs(v) do
+            for i = #computedTables, 1, -1 do
+                if computedTables[i] == computedTable then
+                    table.remove(computedTables, i) -- 逆序遍历，防止删除元素后索引错乱
+                end
+            end
+
+            if #computedTables == 0 then
+                v[key] = nil
+            end
+        end
+
+        if next(v) == nil or k == computedTable then
+            ReactiveSystem.deps[k] = nil
+        end
+    end
+end
+
+-- 传播依赖关系：t可能是proxyTable，也可能是computedTable
+function ReactiveSystem:propgate(t, key)
+    if not self.deps[t] then
         return
     end
 
-    if not self.deps[firstKey][secondKey] then
+    if not self.deps[t][key] then
         return
     end
 
-    if #self.deps[firstKey][secondKey] == 0 then
-        return
-    end
-
-    for _, computedTable in ipairs(self.deps[firstKey][secondKey]) do
-        -- 清除计算属性的缓存
-        self.caches[computedTable] = nil
-
-        -- getter返回nil的标记重置为false
-        computedTable._real.getterValueIsNil = false
-
+    for _, reactiveObj in ipairs(self.deps[t][key]) do
         -- 递归触发依赖关系
-        if isComputed(computedTable) then
-            self:triggerDeps(computedTable, "value")
+        if isComputed(reactiveObj) then
+            reactiveObj._real._recompute = true
+            self:propgate(reactiveObj, "value")
         end
     end
 end
 
 -- 订阅副作用函数
 function ReactiveSystem:subscribe(t, key, effect)
+    if not effect then
+        return
+    end
+
     if not self.effects[t] then
         self.effects[t] = {}
     end
@@ -320,6 +300,11 @@ function ReactiveSystem:subscribe(t, key, effect)
 
     -- 返回一个清理函数
     return function ()
+        if isComputed(t) then
+            self:clearLink(t)
+        end
+
+        -- 取消订阅
         self:unsubscribe(t, key, effect)
     end
 end
@@ -345,13 +330,21 @@ function ReactiveSystem:unsubscribe(t, key, effect)
     for i, v in ipairs(self.effects[t][key]) do
         if v == effect then
             table.remove(self.effects[t][key], i)
-            return
+            break
         end
+    end
+
+    if #self.effects[t][key] == 0 then
+        self.effects[t][key] = nil
+    end
+
+    if #self.effects[t] == 0 then
+        self.effects[t] = nil
     end
 end
 
 -- 清除副作用函数
-local clearEffect = function(effectPtr)
+local stopEffect = function(effectPtr)
     if not effectPtr then
         return
     end
@@ -375,8 +368,8 @@ local clearEffect = function(effectPtr)
     end
 end
 
--- 发布副作用函数
-function ReactiveSystem:publish(t, key, oldValue)
+-- 通知effect重新执行
+function ReactiveSystem:notify(t, key, oldValue)
     if not self.effects[t] then
         return
     end
@@ -419,6 +412,10 @@ watch = function(effect)
     effectFuncPtrStack:push(effect)
     effect()
     effectFuncPtrStack:pop()
+
+    return function()
+        stopEffect(effect)
+    end
 end
 
 -- 取消监视
@@ -442,32 +439,6 @@ computed = function(options)
     return computedTable
 end
 
--- 清理计算属性
-clearComputed = function(computedTable)
-    -- 清理依赖关系
-    for k, v in pairs(ReactiveSystem.deps) do
-        for key, computedTables in pairs(v) do
-            -- 逆序遍历，防止删除元素后索引错乱
-            for i = #computedTables, 1, -1 do
-                if computedTables[i] == computedTable then
-                    table.remove(computedTables, i)
-                end
-            end
-
-            if #computedTables == 0 then
-                v[key] = nil
-            end
-        end
-
-        if next(v) == nil or k == computedTable then
-            ReactiveSystem.deps[k] = nil
-        end
-    end
-
-    -- 清理计算属性的缓存
-    ReactiveSystem.caches[computedTable] = nil
-end
-
 -- VUE3.0中的watch函数, 参数为Ref对象， 默认是懒侦听的，即仅在侦听源发生变化时才执行回调函数。
 watchRef = function(refData, callback)
     return ReactiveSystem:subscribe(refData, "value", function(oldValue)
@@ -484,22 +455,16 @@ end
 
 -- VUE3.0中的watch函数, 参数响应式对象， 默认是懒侦听的，即仅在侦听源发生变化时才执行回调函数。
 watchReactive = function(reactiveObj, callback)
-    -- 存储局部计算属性
-    local computedValues = {}
+    local clears = {}
 
     local watchReactiveImpl = nil
     watchReactiveImpl = function (obj)
         for k, v in pairs(obj._real) do
-            local computedValue = computed(function()
-                return obj[k]
+            local clear = ReactiveSystem:subscribe(obj, k, function(oldValue)
+                callback(k, obj._real[k], oldValue)
             end)
 
-            -- 存储局部计算属性到列表中
-            table.insert(computedValues, computedValue)
-
-            ReactiveSystem:subscribe(obj, k, function(oldValue)
-                callback(computedValue.value, oldValue)
-            end)
+            table.insert(clears, clear)
 
             if isReactive(v) then
                 watchReactiveImpl(v, callback)
@@ -511,8 +476,8 @@ watchReactive = function(reactiveObj, callback)
 
     -- 返回一个清理函数, 当不需要监视时，调用清理函数即可
     return function ()
-        for _, computedValue in ipairs(computedValues) do
-            clearComputed(computedValue)
+        for _, clear in ipairs(clears) do
+            clear()
         end
     end
 end
@@ -520,14 +485,12 @@ end
 return {
     ref = ref,
     reactive = reactive,
-
     computed = computed,
-    clearComputed = clearComputed,
 
     watch = watch,
     unwatch = unwatch,
-    clearEffect = clearEffect,
-    
+    stopEffect = stopEffect,
+
     watchRef = watchRef,
     watchComputed = watchComputed,
     watchReactive = watchReactive,
